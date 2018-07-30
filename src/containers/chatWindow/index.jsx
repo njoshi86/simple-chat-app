d221ca8255bcadc18bfe3840542b063c2ab0a4a9
Nishchal Joshi <njoshi@ten-x.com>
2018-07-29 17:07:11 -0700

    Adding default data

--- src/containers/chatWindow/index.jsx
+++ src/containers/chatWindow/index.jsx
@@ -2,6 +2,31 @@ import React, { Component, PropTypes } from 'react'
 import { connect } from 'react-redux'
 
 class ChatWindow extends Component {
+  static propTypes = {
+    chatRooms: PropTypes.array.isRequired,
+    chatRoomUsers: PropTypes.object.isRequired,
+    chatRoomMessages: PropTypes.object.isRequired
+  }
+  static defaultProps = {
+    chatRooms: [
+      {name: 'Tea Chats', id: 0},
+      {name: 'Coffee Chats', id: 1}
+    ],
+    chatRoomUsers: {
+      0: ['Ryan','Nick', 'Danielle'],
+      1: ['Jessye']
+    },
+    chatRoomMessages: {
+      0: [
+        {name: 'Ryan', message: 'ayyyyy', id: 'gg35545', reaction: null},
+        {name: 'Nick', message: 'lmao', id: 'yy35578', reaction: null},
+        {name: 'Danielle', message: 'leggooooo', id: 'hh9843', reaction: null}
+      ],
+      1: [
+        {name: 'Jessye', message: 'ayy', id: 'ff35278', reaction: null}
+      ]
+    }
+  }
   render () {
     return (
       <div>
