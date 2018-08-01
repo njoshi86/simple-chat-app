b33ce117312855a06df946e6a0435c6a9bb919b9
Nishchal Joshi <njoshi@ten-x.com>
2018-07-31 22:50:02 -0700

    Adding functionality to show how long user has been on the app

--- src/components/userInfo/index.jsx
+++ src/components/userInfo/index.jsx
@@ -1,11 +1,19 @@
 import React, { Component } from 'react'
 import { PropTypes } from 'prop-types'
 import { Header } from 'semantic-ui-react'
+import moment from 'moment'
 import classnames from 'classnames/bind'
 import stylesheet from './styles.scss'
 const cx = classnames.bind(stylesheet)
 
 class UserInfo extends Component {
+  constructor(props) {
+    super(props)
+    this.loginTimeInterval = null
+    this.state = {
+      loginTimeDiff: this.getLoginTimeDiff(props.loginTime)
+    }
+  }
   static propTypes = {
     currentUser: PropTypes.string.isRequired,
     loginTime: PropTypes.string.isRequired
@@ -14,12 +22,44 @@ class UserInfo extends Component {
   static defaultProps = {
     currentUser: ''
   }
+  clearLoginTimeInterval = () => {
+    if (this.loginTimeInterval !== null) {
+      clearInterval(this.loginTimeInterval)
+      this.loginTimeInterval = null
+    }
+  }
+  startInterval = () => {
+    this.clearLoginTimeInterval()
+    this.loginTimeInterval = setInterval(this.updateTimeDiff, 61*1000)
+  }
+  componentDidMount() {
+    this.startInterval()
+  }
+  componentWillUnmount () {
+    this.clearLoginTimeInterval()
+  }
+  updateTimeDiff = () => {
+    this.setState({
+      loginTimeDiff: this.getLoginTimeDiff(this.props.loginTime)
+    }, () => {
+    })
+  }
+  getLoginTimeDiff = (loginTime) => {
+    const currentTimeUTC = moment(moment()).format('YYYY-MM-DD hh:mm:ss Z')
+    const diffInMinutes = parseInt(moment(currentTimeUTC).diff(loginTime, 'minutes'), 10)
+    if (diffInMinutes < 60) {
+      return `Online for ${diffInMinutes > 0 ? diffInMinutes : 1} minutes`
+    } else {
+      const diffInHours = parseInt(moment(currentTimeUTC).diff(loginTime, 'hours'), 10)
+      return `Online for ${diffInHours} hours and ${diffInMinutes - (diffInHours * 60)} minutes`
+    }
+  }
   render () {
-    const { currentUser, loginTime } = this.props
+    const { currentUser } = this.props
     return (
       <div className={cx('user-info-container')}>
         <Header as='h2' textAlign={'left'} className={cx('user-name')}>{currentUser}</Header>
-        <Header as='h5' textAlign={'left'} className={cx('user-login-time')}>{loginTime}</Header>
+        <Header as='h5' textAlign={'left'} className={cx('user-login-time')}>{this.state.loginTimeDiff}</Header>
       </div>
     )
   }
--- src/reducers/initialState.js
+++ src/reducers/initialState.js
@@ -4,5 +4,7 @@ export default {
   chatRoomUsers: {},
   chatRoomMessages: {},
   currentUser: 'Ryan',
-  loginTime: moment(moment()).format('YYYY-MM-DDThh:mm:ss')
+  loginTime: moment(moment()).format('YYYY-MM-DD hh:mm:ss Z')
 }
+
+// 2018-07-31 10:31:52 -07:00
